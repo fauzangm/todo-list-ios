4fe0cd4672c7ee2a0a4006340f540ac85766c476
Fauzan Ghozi Mubarak <fauzanghozi56@gmail.com>
2023-07-02 03:18:55 +0700

    initial project

---
+++ todo-list-app/todo_list_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  todo_list_appApp.swift
+//  todo-list-app
+//
+//  Created by Fauzan Ghozi Mubarak on 01/07/23.
+//
+
+import SwiftUI
+
+@main
+struct todo_list_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
